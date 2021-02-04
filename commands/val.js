a09a3ec957a655c7f50083f73df89f4ce58a863c
Lowe Lowing <lowe.lowing@gmail.com>
2021-02-04 17:43:15 +0100

    New Lol function

--- commands/val.js
+++ commands/val.js
@@ -1,12 +1,11 @@
+const Discord = require('discord.js');
 module.exports = {
 	name: 'val',
 	aliases: ['v'],
 	description: 'Show rank of player',
 	args: true,
-	argsNeeded: false,
+	argsNeeded: true,
 	execute(message, args) {
-		//class="map-countdown"
-		//website = https://apexmap.kuroi.io
 		const acc = args[0].split("#")
 		const fetch = require("node-fetch");
 		const cheerio = require("cheerio");
@@ -14,10 +13,15 @@ module.exports = {
 			fetch("https://tracker.gg/valorant/profile/riot/" + acc[0] +"%23" + acc[1] + "/overview").then(res => res.text())
 			.then(html => {
 				const $ = cheerio.load(html)
-				// console.log(html)
 				const title = $(".valorant-highlighted-stat__value")
 				const og = title.text()
-				message.channel.send(og.substring(0,og.split("").length-4))
+				rank = og.substring(0,og.split("").length-4)
+
+				const embed = new Discord.MessageEmbed()
+				.setTitle("Show players ranks")
+				.setDescription(acc[0] + "'s rank is " + "**" + rank + "**")
+				.setColor("#0036FF")
+				message.channel.send(embed)
 			});	
 		} catch (error) {
 			console.error();
@@ -25,11 +29,3 @@ module.exports = {
 
 	}
 };
-// valorant-highlighted-stat__value
-
-// var str = 1437203995000;
-// str = str.toString();
-// console.log("Original data: ",str);
-// str = str.slice(0, -3);
-// str = parseInt(str);
-// console.log("After truncate: ",str);
