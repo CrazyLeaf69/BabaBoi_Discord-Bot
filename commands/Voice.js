6c23df4a87e1d65b9d33540660c8260596807b7e
Lowe Lowing <lowe.lowing@gmail.com>
2021-03-02 01:18:50 +0100

    try catch

--- commands/Voice.js
+++ commands/Voice.js
@@ -71,70 +71,59 @@ async function SpeechToText(message, server) {
             // message.send(`What I heard: ${transcription}`).catch(console.error);
             console.log(`Transcription: ${transcription}`);
         }
+
+        // ------------------------------------------------------------------------------------------------
+        // --------------------------check the response ---------------------------------------------------
+        // ------------------------------------------------------------------------------------------------
+        // coomands
+        const searchWords = transcription.split(" ").splice(1).join(" ");
+        const word = transcription.split(" ");
+        if (transcription.includes("speak")) {
+            speak(message);
+        }
+        else if (word[0]=="play" && word[1]!="number") {
+            play(searchWords, message, server);
+        }
+        else if (word[0]=="play" && word[1]=="number") {
+            if (word[2]=="one") {
+                queue[0] = {title: searchResults[0].title, url: searchResults[0].url};
+            } else {
+                queue[0] = {title: searchResults[word[2]-1].title, url: searchResults[word[2]-1].url};
+            }
+            console.log(queue);
+            playQueue(message, server);
+        }
+        else if (word[0]=="search") {
+            search(searchWords, message, server);
+        }
+        else if (word[0]=="lägg"&&word[1]=="till"&&word[2]!=undefined) {
+            let number;
+            if (word[2]=="ett") {number = 0;}
+            if (word[2]=="två") {number = 1;}
+            if (word[2]=="tre") {number = 2;}
+            if (word[2]=="fyra") {number = 3;}
+            if (word[2]=="fem") {number = 4;}
+            addToQueue(number);
+        }
+        else if (transcription.includes("skip")||transcription.includes("next")) {
+            try {
+                if (queue.length > 0) {
+                    queue.shift();
+                }
+                playQueue(message, server);
+            } catch (error) {
+                sendToBotChannel(server, "", "Queue empty", "")
+            }
+        }
+        else if (transcription.includes("pause")||transcription.includes("pausa")) {
+            pause(message);
+        }
+        else if (transcription.includes("resume")) {
+            resume(message);
+        }
+        recordAgain(message, server);
     } catch (error) {
         console.log(error);
-    }
-    // ------------------------------------------------------------------------------------------------
-    // --------------------------check the response ---------------------------------------------------
-    // ------------------------------------------------------------------------------------------------
-    // coomands
-    const searchWords = transcription.split(" ").splice(1).join(" ");
-    const word = transcription.split(" ");
-    if (transcription.includes("speak")) {
-        speak(message);
-    }
-    else if (word[0]=="play" && word[1]!="number") {
-        play(searchWords, message, server);
-    }
-    else if (word[0]=="play" && word[1]=="number") {
-        if (word[2]=="one") {
-            queue[0] = {title: searchResults[0].title, url: searchResults[0].url};
-        } else {
-            queue[0] = {title: searchResults[word[2]-1].title, url: searchResults[word[2]-1].url};
-        }
-        console.log(queue);
-        playQueue(message, server);
-        recordAgain(message, server).catch(console.error);
-    }
-    else if (word[0]=="search") {
-        search(searchWords, message, server);
-    }
-    else if (word[0]=="lägg"&&word[1]=="till"&&word[2]!=undefined) {
-        let number;
-        if (word[2]=="ett") {number = 0;}
-        if (word[2]=="två") {number = 1;}
-        if (word[2]=="tre") {number = 2;}
-        if (word[2]=="fyra") {number = 3;}
-        if (word[2]=="fem") {number = 4;}
-        addToQueue(number);
-        recordAgain(message, server).catch(console.error);
-    }
-    else if (transcription.includes("skip")||transcription.includes("next")) {
-        try {
-            if (queue.length > 0) {
-                queue.shift();
-            }
-            playQueue(message, server);
-        } catch (error) {
-            sendToBotChannel(server, "", "Queue empty", "")
-        }
-        // ------------------------------------------------------------------------- testa "next" "next"
-        // playQueue(message, server).catch(dispatcher = "");
-        // queue.shift();
-        recordAgain(message, server).catch(console.error);
-    }
-    else if (transcription.includes("pause")||transcription.includes("pausa")) {
-        pause(message);
-    }
-    else if (transcription.includes("resume")) {
-        resume(message);
-    }
-    else {
-        try {
-            recordAgain(message, server)
-        } catch (error) {
-            console.log(error);
-        }
     }
 }
 
@@ -150,7 +139,6 @@ async function speak(message, server) {
     dispatcher = connection.play(ytdl(url), {filter: 'audioonly', quality: 'highest' });
     dispatcher.on('error', console.error);
     console.log("spoke");
-    recordAgain(message, server)
 }
 async function play(search, message, server) {
     try {
@@ -159,7 +147,7 @@ async function play(search, message, server) {
         url = "https://www.youtube.com/watch?v=NCFg7G63KgI";
         await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${search}&type=video&key=AIzaSyD4q3HFuGrKvo7qpB0-wsJYWnKiWwZGILM`)
         .then(res => res.json()).then(data => {
-            const items = data.items
+            const items = data.items;
             const videoId = items[0].id.videoId;
             url = `https://www.youtube.com/watch?v=${videoId}`;
             console.log(items[0].snippet.title);
@@ -168,7 +156,6 @@ async function play(search, message, server) {
         });
         console.log(queue);
         playQueue(message, server)
-        recordAgain(message, server).catch(console.error);
     } catch(err) {
         console.log(err);
     };
@@ -199,7 +186,6 @@ async function search(search, message, server) {
     } catch(err) {
         console.log(err);
     };
-    recordAgain(message, server).catch(console.error);   
 }
 function addToQueue(number) {
     queue.push({title: searchResults[number].title, url: searchResults[number].url});
@@ -207,11 +193,9 @@ function addToQueue(number) {
 }
 function pause(message, server) {
     dispatcher.pause(message);
-    recordAgain(message, server).catch(console.error);
 }
 function resume(message, server) {
     dispatcher.resume(message);
-    recordAgain(message, server).catch(console.error);
 }
 // ----------------------------------------------------------------------------------
 // play queue
