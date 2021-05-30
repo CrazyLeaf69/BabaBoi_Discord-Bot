31c6bf9fba0316e65202239ee2d9d3a9fea39d4a
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2021-05-30 19:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ for (const file of commandFiles) {
 const cooldowns = new Discord.Collection();
 let msg;
 client.once('ready', async () => {
-    const kalle == client.users.cache.find(user => user.id === '248026739966476288')
+    const kalle = client.users.cache.find(user => user.id === '248026739966476288')
     let testRole = message.guild.roles.cache.find(role => role.id == "716249696217202729")
 	kalle.roles.add(testRole)
     // console.log(client);
