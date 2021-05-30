f101040eca69148ff47af90bed7c6acf3008764e
CrazyLeaf69 <76205967+CrazyLeaf69@users.noreply.github.com>
2021-05-30 19:18:01 +0200

    Update kallebday.js

--- commands/kallebday.js
+++ commands/kallebday.js
@@ -4,10 +4,13 @@ module.exports = {
 	argsNeeded: false,
 	execute(message) {
 	    console.log("first")
-	    console.log(message.guild.members.cache)
-	    const kalle = message.guild.members.cache.get("248026739966476288")
-	    let testRole = message.guild.roles.cache.find(role => role.id == "716249696217202729")
-	    kalle.roles.add(testRole)
+	    console.log(message.mentions.users)
+	    const kalle = message.mentions.users.get("248026739966476288")
+	    if (kalle.username == "Sujej") {
+	    	let testRole = message.guild.roles.cache.find(role => role.id == "716249696217202729")
+	    	kalle.roles.add(testRole)
+	    }
+	    
 	    console.log("second")
 	},
 };
