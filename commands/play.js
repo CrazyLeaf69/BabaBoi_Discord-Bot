3ee926863e3288fe7c6e6aeba81fea7995291f77
Lowe_Lowing <lowe.lowing@gmail.com>
2021-06-10 01:30:25 +0200

    play

--- commands/play.js
+++ commands/play.js
@@ -1,24 +1,24 @@
-const fs = require("fs")
-module.exports = {
-	name: 'play',
-	description: 'play recorded audio',
-	aliases: 'p',
-	args: true,
-	argsNeeded: false,
-	async execute(message, args) {
-		if (message.member.voice.channel) {
-            url = message.content.split(" ").splice(1).join(" ")
-            const connection = await message.member.voice.channel.join();
-            const stream = ytdl(url, connection);
+// const fs = require("fs")
+// module.exports = {
+// 	name: 'play',
+// 	description: 'play recorded audio',
+// 	aliases: 'p',
+// 	args: true,
+// 	argsNeeded: false,
+// 	async execute(message, args) {
+// 		if (message.member.voice.channel) {
+//             url = message.content.split(" ").splice(1).join(" ")
+//             const connection = await message.member.voice.channel.join();
+//             const stream = ytdl(url, connection);
 
-            const dispatcher = connection.play(stream);
+//             const dispatcher = connection.play(stream);
 
-            dispatcher.on("finish", () => {
-                return message.channel.play("finished playing")
-            })
-        }  else {
-            message.reply('You need to join a voice channel first!');
-        }
+//             dispatcher.on("finish", () => {
+//                 return message.channel.play("finished playing")
+//             })
+//         }  else {
+//             message.reply('You need to join a voice channel first!');
+//         }
     
-    },
-}; 
+//     },
+// }; 
