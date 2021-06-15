980f49e3171c60031642ef533350870b85107ca7
Lowe_Lowing <lowe.lowing@gmail.com>
2021-06-15 23:29:43 +0200

    dwd

--- index.js
+++ index.js
@@ -41,10 +41,9 @@ client.on('ready', async () => {
     const date = new Date();
     const sec = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()
     let msg = await client.channels.cache.get("693042214875430957").send('Sekunder tills Lowe fyller 18 år: ' + 86400-sec)
-    console.log();
     setInterval(() => {
         var date = new Date();
-        var seconds = date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()
+        var seconds = date.getHours()-2 * 3600 + date.getMinutes() * 60 + date.getSeconds()
         console.log(seconds);
         msg.edit('Sekunder tills Lowe fyller 18 år: ' + 86400-seconds)
     }, 1000);
