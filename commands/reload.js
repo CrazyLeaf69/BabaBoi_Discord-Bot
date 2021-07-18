fada1d83a0d954ab5c6b95348f5689f236b355f5
Lowe_Lowing <lowe.lowing@gmail.com>
2021-07-18 22:19:38 +0200

    reload fixed but lol command still fucked

--- commands/reload.js
+++ commands/reload.js
@@ -7,7 +7,7 @@ module.exports = {
 	argsNeeded: true,
 	execute(message, args) {
 		if (!args.length) return message.channel.send(`You didn't pass any command to reload, ${message.author}!`);
-		const commandName = args[0].toLowerCase();
+		const commandName = args.toLowerCase();
 		const command = message.client.commands.get(commandName)
 		|| message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 		
